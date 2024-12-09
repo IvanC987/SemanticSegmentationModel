edbffd1f02d277bdeb498d4fa4fa79c83128ccad
Ivan Cao <ivanc135246@gmail.com>
2024-12-09 12:04:14 -0600

    Fixed a minor overlook

--- inference.py
+++ inference.py
@@ -57,7 +57,12 @@ clear_directory(pred_img_dir)
 model_path = "unet_model.pth"
 state_dict = torch.load(model_path, map_location=torch.device(device))
 
-pv_to_class, class_to_pv = pixel_mappings(mask_dir=r"./Dataset/ScaledMasks")
+# To test out the model that I've trained, knowing the pixel_values-to-class mapping is required
+# Due to the Cityscapes dataset being proprietary, the Masks cannot be distributed, hence I've statically included the two variable below for inferencing
+# If using your trained model, just comment it out
+# pv_to_class, class_to_pv = pixel_mappings(mask_dir=r"./Dataset/ScaledMasks")
+pv_to_class={(0, 0, 0): 0, (70, 70, 70): 1, (153, 153, 153): 2, (70, 130, 180): 3, (107, 142, 35): 4, (220, 220, 0): 5, (190, 153, 153): 6, (250, 170, 30): 7, (220, 20, 60): 8, (111, 74, 0): 9, (0, 0, 142): 10, (128, 64, 128): 11, (244, 35, 232): 12, (250, 170, 160): 13, (81, 0, 81): 14, (152, 251, 152): 15, (119, 11, 32): 16, (255, 0, 0): 17, (102, 102, 156): 18, (0, 60, 100): 19, (150, 100, 100): 20, (0, 0, 230): 21, (0, 80, 100): 22, (0, 0, 70): 23}
+class_to_pv={0: (0, 0, 0), 1: (70, 70, 70), 2: (153, 153, 153), 3: (70, 130, 180), 4: (107, 142, 35), 5: (220, 220, 0), 6: (190, 153, 153), 7: (250, 170, 30), 8: (220, 20, 60), 9: (111, 74, 0), 10: (0, 0, 142), 11: (128, 64, 128), 12: (244, 35, 232), 13: (250, 170, 160), 14: (81, 0, 81), 15: (152, 251, 152), 16: (119, 11, 32), 17: (255, 0, 0), 18: (102, 102, 156), 19: (0, 60, 100), 20: (150, 100, 100), 21: (0, 0, 230), 22: (0, 80, 100), 23: (0, 0, 70)}
 
 unet = UNET(in_channels=3, out_channels=len(class_to_pv)).to(device)  # Adjust out_channels accordingly
 unet.load_state_dict(state_dict)
